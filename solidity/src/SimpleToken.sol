a3af52b706cdbcc7ee229fc2d6cae1ef9ba92a38
Chaitanya Potti <chaitanya.potti@gmail.com>
2024-11-16 13:25:29 +0800

    add helper contracts

---
+++ solidity/src/SimpleToken.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.6;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract SimpleToken is ERC20, Ownable {
+    constructor() ERC20("Simple Token", "SIMPL") Ownable(msg.sender) {
+        _mint(msg.sender, 100000 * 10**18);
+    }
+
+    function mint(address to, uint256 amount) external onlyOwner {
+        _mint(to, amount);
+    }
+}
